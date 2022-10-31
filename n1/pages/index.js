33a65283abd740c0a5a0599c1000379083c2c304
Kaion-art <kaionmuriloal@gmail.com>
2022-10-31 08:57:19 -0300

    problemas no axios e next

--- n1/pages/index.js
+++ n1/pages/index.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
+import api from './api'
+
+
+function getUser(){
+  api.get("https://backendtalentos.herokuapp.com/listuser")
+      .then(Response =>{
+          const date =Response.data
+          redemeResults.textCont=JSON.stringify(date)
+      })
+      
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+     });
+
+}
+getUser()
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>hello World NEXt.js </h1>
+      <h1>hello World NEXt.js oi</h1>
+      
     </div>   
   )
 }
